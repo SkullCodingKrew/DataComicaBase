f16ecda7e207811d0ec6778bf543bf01842500bb
topokun <rafpen78@gmail.com>
2015-10-12 17:58:45 +0200

    Finished parsing first version

--- ComicBook.py
+++ ComicBook.py
@@ -7,10 +7,13 @@ related methods and types
 ##############################################
 
 ### Python Default modules ###################
+### Regular expressions
+import re
 
 ### Python SQLObject #########################
 from sqlobject import *
-from sqlobject.col import StringCol
+from sqlobject.col import StringCol, ForeignKey
+from sqlobject.sresults import SelectResults
 
 ##############################################
 ### Classes                                ###
@@ -24,7 +27,9 @@ class Author(SQLObject):
     middleName = StringCol(default=None)
     lastName   = StringCol()
   
-    comicBook  = RelatedJoin('ComicBook')
+    comicBook  = SQLRelatedJoin('ComicBook',
+            intermediateTable='comic_authors',
+            createRelatedTable=False)
     
     def print_info (self):
         """ Print the comic object to the terminal """
@@ -39,19 +44,40 @@ class Author(SQLObject):
 class Character(SQLObject):
     """ Character class, to be explained ;) """
     
-    ## Name Columns Middle is optional
+    ## Alias (name of the character)
     alias = StringCol()
     
     ## To be added like above?
     real_name = StringCol(default=None) 
 
     ## N to N relationship
-    comicBook  = RelatedJoin('ComicBook')
+    comicBook  = SQLRelatedJoin('ComicBook',
+            intermediateTable='comic_characters',
+            createRelatedTable=False)
     
     def print_info (self):
         """ Print the comic object to the terminal """
         print "############################################"
         print "Alias: "        + self.alias
+
+### Groups ####################################
+class CharGroup(SQLObject):
+    """ Group class, to be explained ;) """
+    
+    ## Alias (name of the character)
+    name = StringCol()
+    
+    ## To be added like above?
+    roster = StringCol(default=None) 
+
+    ## N to N relationship
+    comicBook  = SQLRelatedJoin('ComicBook',
+            intermediateTable='comic_groups',
+            createRelatedTable=False)
+    
+    def print_info (self):
+        """ Print the comic object to the terminal """
+        print "############################################"
 
 ### StoryArc ##################################
 class StoryArc(SQLObject):
@@ -65,6 +91,17 @@ class StoryArc(SQLObject):
     def print_info (self):
         """ Print the comic object to the terminal """
 
+### Universe ##################################
+class Universe(SQLObject):
+    """ Author class, to be explained ;) """
+    
+    ## Name Columns Middle is optional
+    name  = StringCol()
+
+    comicBook  = RelatedJoin('ComicBook')
+    
+    def print_info (self):
+        """ Print the comic object to the terminal """
 
 ### ComicBook ################################
 class ComicBook(SQLObject):
@@ -81,19 +118,29 @@ class ComicBook(SQLObject):
     
     ## Story Related
     storyName     = StringCol()
-    storyArc      = ForeignKey("StoryArc", default=None)
+    storyArc      = StringCol(default=None)
+    ## TODO: Do we need an extra table?
+    #storyArc      = ForeignKey("StoryArc", default=None)
     storySynopsys = StringCol(default=None)
-    character     = RelatedJoin('Character')
+    character     = SQLRelatedJoin('Character',
+                                   intermediateTable='comic_characters',
+                                   createRelatedTable=False)
+
+    group         = SQLRelatedJoin('CharGroup',
+                                   intermediateTable='comic_groups',
+                                   createRelatedTable=False)
+    
+    universes      = StringCol(default=None)
+    ## TODO: Do we need an extra table?
+    #Universe      = ForeignKey("Universe")
     
     ## Order of the Comic for reading
     orderNumber   = IntCol()
     
     ## Author link info (n-n relationship)
-    writer    = RelatedJoin('Author') 
-    penciller = RelatedJoin('Author')
-    inker     = RelatedJoin('Author')
-    collorist = RelatedJoin('Author')
-    letterer  = RelatedJoin('Author')
+    authors = SQLRelatedJoin('Author',
+            intermediateTable='comic_authors',
+            createRelatedTable=False)
     
     ## Publishing info 
     coverDate     = DateCol(default=None)
@@ -105,10 +152,124 @@ class ComicBook(SQLObject):
     ## Readed already
     readed    = BoolCol(default=False)
 
-    def add_info (self,key,value):
-    #    """ Add some info to the Comic with a key/value par """
-    #    self.info[key] = value
-        True
+    def add_authors (self,authors_dict):
+        """ Add authors to the comicbook """
+        ### Split for authors
+        for key,value in (authors_dict.items()):
+            authors = value.split(",")
+        
+            ## Loop for each author
+            for author_str in authors:
+                ## Create arguments for Author Object
+                name_args = {}
+            
+                ## Strip spaces at the beguining and end
+                author_str = author_str.strip()
+            
+                #Split the name in its components
+                if (not author_str):
+                    print "Author is missing %s" % (key)
+                if (author_str):
+                    names = author_str.split(" ")
+                    name_args['firstName'] = names[0]
+                    if (len(names) == 2):
+                        name_args['lastName'] = names[1]
+                    elif (len(names) == 3):
+                        name_args['lastName'] = names[2]
+                        name_args['middleName'] = names[1]
+                
+                    ## Search for Author in database
+                    Author_search = Author.selectBy(**name_args)
+                    try:
+                        myAuthor = Author_search.getOne(None)
+                    except Exception as e:
+                        ## Catch duplicates entry and report
+                        print e
+                        print "Error : Duplicated entry for %s" % name_args
+            
+                    ## If Author not found create a new entry 
+                    if (not myAuthor):
+                        myAuthor = Author(**name_args)
+            
+                    ## Add relationship author/comic
+                    myComicAuthor = ComicAuthors(comic=self.id,
+                                             author=myAuthor.id,
+                                             role=key)   
+
+    def add_characters (self,characters_dict):
+        """ Add characters to the comicbook """
+        
+        ### Loop through the characters type
+        for role,characters in (characters_dict.items()):
+            
+            ## Loop for each character
+            for idx in range(len(characters)/2):
+                ## Create arguments for Author Object
+                char_args = {}
+                
+                ## Alias & Description
+                char_args["alias"] = characters[idx*2]
+                comment = characters[idx*2+1] 
+                       
+                ## Search for Author in database
+                Character_search = Character.selectBy(**char_args)
+                try:
+                    myCharacter = Character_search.getOne(None)
+                except Exception as e:
+                    ## Catch duplicates entry and report
+                    print e
+                    print "Error : Duplicated entry for %s" % char_args
+            
+                ## If Character not found create a new entry 
+                if (not myCharacter):
+                    myCharacter = Character(**char_args)
+            
+                ## Add relationship author/comic
+                myComicCharacter = ComicCharacters(comic=self.id,
+                                                   character=myCharacter.id,
+                                                   comment=comment,
+                                                   role=role)   
+
+    def add_groups (self,groups_list):
+        """ Add groups to the comicbook """
+        
+        ## Loop for each character
+        for idx in range(len(groups_list)/2):
+                
+                ## Create arguments for Group Object
+                group_args = {}
+                
+                ## Check if roster defined
+                roster = None
+                name = groups_list[idx*2]
+                match_roster = re.search("\((.*)\)",name)
+                if (match_roster):
+                    roster = match_roster.group(1)
+                    name = name.replace(match_roster.group(0),"")
+                
+                ## Create arguments    
+                group_args['name'] = name.strip()
+                group_args['roster'] = roster
+                comment = groups_list[idx*2+1] 
+                       
+                ## Search for Group in database
+                Group_search = CharGroup.selectBy(**group_args)
+                try:
+                    myGroup = Group_search.getOne(None)
+                except Exception as e:
+                    ## Catch duplicates entry and report
+                    print e
+                    print "Error : Duplicated entry for %s" % group_args
+            
+                ## If Character not found create a new entry 
+                if (not myGroup):
+                    myGroup = CharGroup(**group_args)
+            
+                ## Add relationship author/comic
+                myComicGroup = ComicChargroups(comic=self.id,
+                                               group=myGroup.id,
+                                               comment=comment)   
+
 
     def print_info (self):
         """ Print the comic object to the terminal """
@@ -128,5 +289,41 @@ class ComicBook(SQLObject):
         #for key,val in self.info.iteritems():
         #    print key + ": " + val
 
+### ComicAuthors ###################################
+class ComicAuthors(SQLObject):
+    """ Comic Author relationship class, to be explained ;) """
+    class sqlmeta:
+         table = "comic_authors"
+    
+    ## Correlation table rows
+    comic  = ForeignKey('ComicBook', notNull=True, cascade=True)
+    author = ForeignKey('Author', notNull=True, cascade=True)
+    role   = StringCol(notNull=True)
+    unique = index.DatabaseIndex(comic, author, role, unique=True)
+    
+### ComicCharacter ###################################
+class ComicCharacters(SQLObject):
+    """ Comic Author relationship class, to be explained ;) """
+    class sqlmeta:
+         table = "comic_characters"
+    
+    ## Correlation table rows
+    comic     = ForeignKey('ComicBook', notNull=True, cascade=True)
+    character = ForeignKey('Character', notNull=True, cascade=True)
+    role      = StringCol(notNull=True)
+    comment   = StringCol(default=None)
+    unique    = index.DatabaseIndex(comic, character, unique=True)
+
+### ComicCharacter ###################################
+class ComicChargroups(SQLObject):
+    """ Comic Groups relationship class, to be explained ;) """
+    class sqlmeta:
+         table = "comic_chargroups"
+    
+    ## Correlation table rows
+    comic     = ForeignKey('ComicBook', notNull=True, cascade=True)
+    group     = ForeignKey('CharGroup', notNull=True, cascade=True)
+    comment   = StringCol(default=None)
+    unique    = index.DatabaseIndex(comic, group, unique=True)
     
 
--- main.py
+++ main.py
@@ -6,8 +6,10 @@
 ### Python Default modules ###################
 from sys import argv
 import os
+from datetime import datetime
+
 ## Beatiful soup import
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup,SoupStrainer
 
 ## For opening urls
 import urllib2
@@ -19,7 +21,15 @@ import re
 from sqlobject import *
 
 ### ComicBook classes ########################
-from ComicBook import ComicBook
+from ComicBook import *
+from BeautifulSoup import ResultSet
+
+##############################################
+### Global values                         ####
+##############################################
+authors_list = ("Editor-in-chief","Cover Artists","Writers","Pencilers","Inkers","Colourists","Letterers","Editors")
+                
+                
 
 ##############################################
 ### Compiled regex                        ####
@@ -46,23 +56,175 @@ key_value_re = re.compile("^\s*(.+)\s*:\s*\n*\s*(.+)\s*$")
 ##############################################
 ### Defined methods                       ####
 ##############################################
-def get_comic_title (text_string):
-    """ Parses string and returns a tuple containing: 
+def get_comic_title (main_table_item):
+    """ Get the info in the comic title from the html main_table  returns a tuple containing: 
         Title  => Title of the comic book serie
         number => Number of the comic book in current Volume
         part   => Part inside the comic book (For comic books with 2 or 3 different stories
         volume => Volume of the comic book title
         order  => Number in reading order (optional)
     """
-      
-    match = title_re.match(text_string)
+    
+    ## Define an empty dictionary
+    comic_kwargs = {}
+    
+    ## Title is the only h1 tag within the main table html
+    comic_title_tags = main_table_item.select("h1")
+    
+    ## If we found  an h1 on the html
+    if (comic_title_tags) :
+        
+        ## Parse the whole text with a regular expresion
+        match = title_re.match(comic_title_tags[0].get_text())
 
-    if (match):
-        return match.group("title"),match.group("number"),match.group("part"),match.group("vol_n"),match.group("order_n")
+        ## Check if there was a match or not
+        if ( not match):
+            print "No match for title found in %s" % (comic_title_tags[0].get_text())
+            return None
+        
+        ## Process the match and create the dict for initializating the comic book  
+        comic_kwargs['comicTitle']  = match.group("title")
+        comic_kwargs['issue']       = int(match.group("number"))
+        comic_kwargs['part']        = match.group("part")
+        comic_kwargs['volume']      = int(match.group("vol_n"))
+        comic_kwargs['orderNumber'] = int(match.group("order_n"))
+        
+        ### Return the dict
+        return comic_kwargs
+    
     else:
-        print "No match for title in %s" % (text_string)
-        return None,None,None,None
+        print "No h1 found on following table html %s" % (main_table_item)
+        return None
+        
+def get_comic_story (story_table_item):
+    """ Get story description from main table """
+    
+    ## Define an empty dictionary
+    comic_kwargs = {}
+        
+    try:
+        story_name_tags = story_table_item.td.div
+    except Exception as e:
+        print e
+        print "Error : False story table item \n %s" % (story_table_item)
+        return
+            
+    ## If found add history
+    if (story_name_tags):
+        comic_kwargs['storyName'] = story_name_tags.get_text().replace('"','')
+        
+    return comic_kwargs
 
+def get_comic_details (details_table_item):
+    """ Get comic details from main table """
+    ## Define an empty dictionary
+    details_dict = {}
+    
+    ## Check for TDs on the tr, the second one is the one with the tr
+    details_td = details_table_item.find_all("td", recursive=False)
+    
+    try:
+        details_table = details_td[1].table
+        details_items = details_table.find_all("td")
+    except Exception as e:
+        print e
+        print "Error : False details section selected \n %s" %(details_table_item)
+        return
+    
+    for detail in (details_items) :
+        detail_match = key_value_re.match(detail.get_text())
+        if (detail_match):
+            details_dict[detail_match.group(1)] = detail_match.group(2)
+        
+    return details_dict
+
+def get_comic_synopsys (story_table_item):
+    """ Get story synopsys from main table """
+    
+    ## Define an empty dictionary
+    comic_kwargs = {}
+        
+    try:
+        synopsys_tags = story_table_item.td
+    except Exception as e:
+        print e
+        print "Error : False synposys table item \n %s" % (story_table_item)
+        return
+            
+    ## If found add history
+    if (synopsys_tags):
+        comic_kwargs['storySynopsys'] = synopsys_tags.get_text()
+                   
+    return comic_kwargs
+
+def get_characters_groups (char_table_item):
+    """ Get characters from main table """
+    
+    ## Define an empty dictionary
+    char_group_kwargs = {}
+        
+    try:
+        ## This is the Main Sub  Table for characters and comments
+        sub_main_table = char_table_item.td.div.table
+        sub_main_item = sub_main_table.find_all("tr",recursive=False)
+        
+        ## This is the Main Character Table
+        char_main_table = sub_main_item[1].td.table
+        char_main_items = char_main_table.find_all("tr",recursive=False)
+        
+        ## Find the character and group subtables
+        key = ""
+        char_group_table = {}
+        for tr in (char_main_items):
+            char_text = tr.get_text()
+            char_text = char_text.strip()
+            if (char_text == "Character Appearances"):
+                key = "characters"
+            elif (char_text == "Group Appearances"):
+                key = "groups"
+            else:
+                if (key):
+                    ## There are one table for each type of char/group
+                    char_group_table[key] = []
+                    tables = tr.find_all("table")
+                    for table in (tables):
+                        char_group_table[key].append(table) 
+                    key = None
+        
+        ## Loop into the characters table to get characters
+        if ("characters" in char_group_table):
+            char_group_kwargs["characters"] = {}
+            for table in (char_group_table["characters"]):
+                tr_items = table.find_all("tr",recursive=False)
+                title = tr_items[0].get_text().strip()
+                del tr_items[0]
+                char_group_kwargs["characters"][title] = []
+                ## Get pairs of name and desc for characters
+                for tr in (tr_items):
+                    tr_text = tr.get_text().strip()
+                    if (tr_text and tr_text !="\n"): 
+                        char_group_kwargs["characters"][title].append(tr_text)
+        
+        ## Loop into the groups to get the groups
+        if ("groups" in char_group_table):
+            char_group_kwargs["groups"] = []
+            table  = char_group_table["groups"][0]
+            tr_items = table.tr.td.table.find_all("tr",recursive=False)
+            ## Get pairs of name and desc for groups (Roster can be processed later)
+            for tr in (tr_items):
+                tr_text = tr.get_text().strip()
+                if (tr_text and tr_text !="\n"): 
+                    char_group_kwargs["groups"].append(tr_text)
+                    
+    ## TODO: Improve Exception handling
+    except Exception as e:
+        print e
+        print "Error : False character table  \n %s" % (char_table_item)
+        return
+    
+    return char_group_kwargs
+
+        
 ##############################################
 ### Main                                  ####
 ##############################################
@@ -71,59 +233,124 @@ db_filename = os.path.abspath('comicdatabase.sqlite')
 if os.path.exists(db_filename):
     os.unlink(db_filename)
 
+## Connect SQL Lite
 connection_string = 'sqlite:' + db_filename
 connection = connectionForURI(connection_string)
 sqlhub.processConnection = connection
 
+## Create Comic Book table if not present
+ComicBook.createTable(ifNotExists=True)
+Author.createTable(ifNotExists=True)
+ComicAuthors.createTable(ifNotExists=True)
+Character.createTable(ifNotExists=True)
+ComicCharacters.createTable(ifNotExists=True)
+CharGroup.createTable(ifNotExists=True)
+ComicChargroups.createTable(ifNotExists=True)
+
 ## Process each file
 for file_path in (argv[1:]):
     if os.path.isfile(file_path):
+        
+        ## Open file and process it with beatiful Soup
+        ############################################################################
         fh = open(file_path,'r')
         print "Processing : %s" % (file_path)
-        soup = BeautifulSoup(fh,'lxml')
-
+        
+        ## Use a parser to get rid of everything but the tables we want
+        only_tables = SoupStrainer('table')
+        soup = BeautifulSoup(fh,'lxml', parse_only=only_tables)
+        
         ## Main table containing issue and ratings
-        main_table, = soup.select("body > table:nth-of-type(2)")
+        try:
+            main_comic_table = soup.contents[2].tr.td.table 
+        except:
+            print "Error : No well formed CMRO html"
+            exit
+        
+        main_comic_items = main_comic_table.find_all("tr",recursive=False)
+        
+        ## Check if essential issue is there (used for recalculate tr index)
+        ############################################################################
+        idx_inc = 0
+        if (re.search("Essential\s*Issue",main_comic_items[1].get_text())):
+            idx_inc = 1
 
-        ## Main Comic table containing all comic info
-        main_comic_table = main_table.tr.td.table
+        ## Get the intial ComicBook keyword arguments from the main comic table
+        ############################################################################
+        comic_kwargs = get_comic_title(main_comic_items[2+idx_inc])
+        
+        if (not comic_kwargs) :
+            print "Error : No comic found for %s" % (file_path)
+            next
+        
+        ## Add the Story (4 tr on main table)
+        ############################################################################
+        add_kwargs = get_comic_story(main_comic_items[3+idx_inc])
+        
+        if (not add_kwargs):
+            print "Error : No story found for %s" % (file_path)
+        else:    
+            comic_kwargs.update(add_kwargs)
+        
+        ## Add the details (7 tr on main table)
+        ############################################################################
+        details_dict = get_comic_details(main_comic_items[6+idx_inc])
+        authors_dict = {}
+        
+        ## Parse details
+        for detail,value in (details_dict.items()):
+            value = value.strip()
+            ## Check if it is an author
+            if (detail in authors_list):
+                ## Save for later
+                authors_dict[detail] = value
+            else:
+                ## Convert to specific formats
+                if (detail =="Cover Date"):
+                    comic_kwargs["coverDate"] = datetime.strptime(value, "%B %Y")
+                elif (detail == "Release Date"):
+                    comic_kwargs["releaseDate"] = datetime.strptime(value, "%B %Y")
+                elif (detail == "Pages"):
+                    comic_kwargs["numberOfPages"] = int(value)
+                elif (detail == "Cover Price"):
+                    value = value.replace("$","")
+                    comic_kwargs["coverPrice"] = float(value)  
+                elif (detail == "Story Arc"):
+                    if (value != "-"):
+                        comic_kwargs["storyArc"] = value
+                elif (detail == "Universes"):
+                        comic_kwargs["universes"] = value.replace(u'\xa0', u' ')
 
-        ## Get Title and order number
-        comic_title_tags = main_table.select("h1")
-        title,number,part,volume,order = get_comic_title(comic_title_tags[0].get_text())
-        number = int(number)
-        volume = int(volume)
-        order  = int(order)
+        ## Add the Synopsys (15 tr on table)
+        ############################################################################
+        add_kwargs = get_comic_synopsys(main_comic_items[13+idx_inc])
 
-        ## Get StoryName needs a better navigation 
-        story_name_tags = main_comic_table.select('div')
+        if (not add_kwargs):
+            print "Error : No story found for %s" % (file_path)
+        else:
+            comic_kwargs.update(add_kwargs)
 
-        story_name = story_name_tags[0].get_text().replace('"','')
-
-
-        ## Create Comic Book table if not present
-        ComicBook.createTable(ifNotExists=True)
-
+        ## Add the Characters (17 tr on table)
+        ############################################################################
+        char_grp_dict = get_characters_groups(main_comic_items[15+idx_inc])
+        
         ## Create Comic Book object
-        ## TODO: Implement directly as a tuple
-        myComic = ComicBook(comicTitle=title,issue=number,volume=volume,orderNumber=order,storyName=story_name,part=part)
+        ############################################################################
+        myComic = ComicBook(**comic_kwargs)
+        
+        ## Add authors
+        ############################################################################
+        myComic.add_authors(authors_dict)
+        
+        ## Add characters
+        ############################################################################
+        if ("characters" in char_grp_dict):
+            myComic.add_characters(char_grp_dict["characters"])
 
-        ## Get all issues details
-        issue_details = main_comic_table.select('.issue_detail_section')
-        for span in issue_details:
-            ## Match Key: Value pattern
-            detail_match = key_value_re.match(span.parent.get_text())
-
-            ## Check if match happened
-            if (detail_match):
-                ## TODO: Check for subdetails in value for key: value key: value strings
-
-                ## Add first and second parenthesized subgroup as key value
-                myComic.add_info(detail_match.group(1),detail_match.group(2))
-
-
-        ## Print comic info
-        myComic.print_info()
+        ## Add groups
+        ############################################################################
+        if ("groups" in char_grp_dict):
+            myComic.add_groups(char_grp_dict["groups"])
 
 ## THE END of main
 print "Finished parsing the html files"
